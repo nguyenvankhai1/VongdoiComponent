a853c4e900643a9c70e17db28046856871ff78ae
nguyenvankhai1 <59248489+nguyenvankhai1@users.noreply.github.com>
2020-01-03 09:41:33 +0700

    Vongdoi

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <hienThi/> -->
+    <VongDoi/>
+
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+// import hienThi from './components/hienThi'
+
+import VongDoi from './components/VongDoi'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+    // hienThi
+    VongDoi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VongDoi.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="id">
+        <h1>{{value}}</h1>
+        <button v-on:click="update()">undate</button>
+        <button c-on:click="destroyEvent">destroy</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'id',
+    data(){
+        return{
+            value:'giá trị ban đầu '
+        }
+    },
+
+    beforeCreate() {
+        alert('beforeCreate!')
+    },
+    created(){
+        alert('created')
+    },
+    beforeMount(){
+        alert('beforeMount')
+    },
+    mounted(){
+        alert('mounted')
+    },
+    beforeUpdate(){
+        alert('beforeUpdate')
+    },
+    updated(){
+        alert('updated')
+    },
+    beforeDestroy(){
+        alert('beforeDestroy')
+    },
+    destroyed(){
+        alert('destroyed')
+    },
+    methods:{
+        destroyEvent: function(){
+            this.$destroy();
+        },
+        update: function(){
+               this.value = 'giá trị đã được update!!!!'
+        }
+    }
+
+}
+</script>
+<style>
+
+</style>
